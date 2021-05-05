2752169e4d931425e732c4600eeb3bf7ff0231a4
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-05-05 13:59:05 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<div>
+
+# JAK-Discord-Bot
+
+</div>
+
+Invite [Bot](https://discord.com/oauth2/authorize?client_id=756402881913028689&scope=bot) to your Server
+
+## Steps
+
+### Clone the Repository
+To Clone this Repository, open a terminal in a empty folder and type 
+```bash
+git clone https://github.com/Jonak-Adipta-Kalita/JAK-Discord-Bot.git
+```
+
+### Installing The Required Modules
+To install the required modules, just open a terminal in the directory where this project is cloned. Now type: 
+```bash
+pip install virtualenv
+virtualenv venv
+.\venv\Scripts\activate
+pip install -r .\requirements.txt
+``` 
+and hit enter.
+
+### Starting the Bot:
+Create `token.txt` and `prefixes.json` files in the cloned directory. Put `{}` in the 
+`prefixes.json` file. Now go to 
+[Discord Developers Portal](https://discord.com/developers/applications) and create a new 
+application. Now go to the Bot section and create a new Bot. Now copy the Token given in the 
+redirected page and paste it in the `token.txt` file. Now go to the OAuth2 section and copy 
+the CLIENT ID. Now open a new tab in your browser and type 
+`https://discord.com/api/oauth2/authorize?client_id=<YOUR_CLIENT_ID>&scope=bot` and hit enter. 
+Now add the Bot to your server. After you added the bot, open any terminal in the directory 
+and type 
+```bash
+python main.py
+```
+
+## Contributors
+<a href = "https://github.com/Jonak-Adipta-Kalita/JAK-Discord-Bot/graphs/contributors">
+  <img src = "https://contrib.rocks/image?repo=Jonak-Adipta-Kalita/JAK-Discord-Bot"/>
+</a>
---
+++ main.py
@@ -0,0 +1,129 @@
+import discord
+import json
+import random
+import os
+
+from discord.ext import commands
+from discord.ext import tasks
+
+def get_prefix(client, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
+
+client = commands.Bot(command_prefix = get_prefix)
+client.remove_command('help')
+
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+token = read_token()
+
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    prefixes[str(guild.id)] = '!JAK '
+    with open('prefixes.json', 'w') as f:
+        json.dump('prefixes', 'f', indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    prefixes.pop(str(guild.id))
+    with open('prefixes.json', 'w') as f:
+        json.dump('prefixes', 'f', indent=4)
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('!JAK help'))
+    print('Starting The Bot...')
+    print('Bot Started...')
+    print('Running Bot...')
+
+@client.command()
+async def help(ctx):
+    await ctx.send(r"""1. `!JAK ` :Default Prefix
+2. `!JAK help` :Show all the commands
+3. `!JAK ping` :Show the ping
+4. `!JAK 8ball <question>` :Play 8ball game
+5. `!JAK clear <amount>` :Delete messages as given amount
+6. `!JAK kick <@member> reason=<reason>` :kick member/bot
+7. `!JAK ban <@member> reason=<reason>` :ban member/bot
+8. `!JAK unban <member,tag>` :unban member/bot
+9. `!JAK show_rules` :Show the rules """)
+
+@client.command()
+async def show_rules(ctx):
+    await ctx.send(r"""1. Be Cool
+2. Be Awesome
+3. Support Each Other
+4. Don't Betray Anybody
+5. Always Support YOUTUBE not TIKTOK
+6. Dont Ask For Roles """)
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'{member.mention} is banned')
+
+@client.command()
+async def unban(ctx, *, member):
+    banned_user = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    for ban_entry in banned_user:
+        user = ban_entry.user
+        if (user.name, user.discriminator) == (member_name, member_discriminator) :
+            await ctx.guild.unban(user)
+            await ctx.send(f'{member.mention} is unbanned')
+            return
+
+@client.command()
+async def clear(ctx, amount : int):
+    await ctx.channel.purge(limit=amount)
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Ping: {round(client.latency * 1000)}')
+
+@client.event
+async def on_member_join(member, ctx):
+    await ctx.send(f'{member} Just Joined')
+
+@client.event
+async def on_member_remove(member, ctx):
+    await ctx.send(f'{member} Just Left What A Shame 🤣🤣🤣')
+
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                'It is decidedly so.',
+                'Without a doubt.',
+                'Yes – definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'Outlook good.',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                "Don't count on it.",
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtfut.',
+                'Kinda Lazy to answer.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+client.run(token)
