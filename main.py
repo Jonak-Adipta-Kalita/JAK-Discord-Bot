bad377ebbed5770be700e7816aae3fa527ccf952
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-11-10 22:52:19 +0530

    changed some stuffs

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from discord.ext import commands
-import discord, credentials
+import discord, credentials, os
 import src.variables as variables
 
 
 class JAKDiscordBot(commands.Bot):
-    def __init__(self, command_prefix, **options):
-        super().__init__(command_prefix, **options)
+    def __init__(self, command_prefix, intents):
+        super().__init__(
+            command_prefix=command_prefix, intents=intents, help_command=None
+        )
+
+        self.COGS: list = list()
+
+        for files in os.listdir(f"./src/cogs/"):
+            if files.endswith(".py"):
+                self.load_extension(f"src.cogs.{files[:-3]}")
 
 
 if __name__ == "__main__":
-    extensions = ["normal", "events", "moderation", "tictactoe", "8ball", "music"]
-
     bot = JAKDiscordBot(command_prefix=variables.PREFIX, intents=discord.Intents.all())
-    bot.remove_command("help")
-
-    for extension in extensions:
-        try:
-            bot.load_extension(f"src.cogs.{extension}")
-        except Exception as e:
-            print(f"Error: {e}")
-
     bot.run(credentials.TOKEN)
