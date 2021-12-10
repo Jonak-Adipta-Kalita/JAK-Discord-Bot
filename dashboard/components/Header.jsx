851396bb6b95f3a521f02ad9a589383cf93a12b1
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-10 12:26:38 +0530

    implemented authentication with next auth for dashboard

--- dashboard/components/Header.jsx
+++ dashboard/components/Header.jsx
@@ -1,10 +1,11 @@
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 
-const Header = () => {
+const Header = ({ providers }) => {
     const router = useRouter();
-
-    const login = (e) => {};
+    const { data: session } = useSession();
+    const provider = Object.values(providers).map((provider) => provider);
 
     return (
         <header className="flex items-center bg-[#272934] sticky top-0 text-gray-400 p-4 py-5 justify-between shadow-xl">
@@ -21,13 +22,21 @@ const Header = () => {
                 />
                 <h1 className="font-bold text-white">JAK Discord Bot</h1>
             </div>
-            <div className="mr-4">
-                <button
-                    className="cursor-pointer border-[0.1px] border-gray-400 p-4 px-10 rounded-lg hover:scale-125 transition transform duration-100 ease-out focus:outline-none focus:ring-2"
-                    onClick={login}
-                >
-                    Login
-                </button>
+            <div className="">
+                {!session ? (
+                    <div className="mr-4">
+                        <button
+                            className="cursor-pointer border-[0.1px] border-gray-400 p-4 px-10 rounded-lg hover:scale-125 transition transform duration-100 ease-out focus:outline-none focus:ring-2"
+                            onClick={() =>
+                                signIn(provider[0].id, { callbackUrl: "/" })
+                            }
+                        >
+                            Login
+                        </button>
+                    </div>
+                ) : (
+                    <div className=""></div>
+                )}
             </div>
         </header>
     );
--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -1,17 +1,26 @@
+import { getProviders } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
 
-const Home = () => {
+const Home = ({ providers }) => {
     return (
         <div className="h-screen overflow-x-scroll scrollbar-hide">
             <Head>
                 <title>JAK Discord Bot - Dashboard</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Header />
+            <Header providers={providers} />
             <main className=""></main>
         </div>
     );
 };
 
 export default Home;
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: { providers },
+    };
+}
