be37fb2edb1bdcb4fcf767e3a09ab67cf23c91b8
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-10 09:56:01 +0530

    does the header look good?

--- dashboard/components/Header.jsx
+++ dashboard/components/Header.jsx
@@ -1,5 +1,36 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
 const Header = () => {
-    return <div></div>;
+    const router = useRouter();
+
+    const login = (e) => {};
+
+    return (
+        <header className="flex items-center bg-[#272934] sticky top-0 text-gray-400 p-4 py-5 justify-between shadow-xl">
+            <div
+                onClick={() => router.push("/")}
+                className="flex items-center space-x-4"
+            >
+                <Image
+                    src="/favicon.ico"
+                    alt="logo"
+                    height={60}
+                    width={60}
+                    className="cursor-pointer"
+                />
+                <h1 className="font-bold text-white">JAK Discord Bot</h1>
+            </div>
+            <div className="mr-4">
+                <button
+                    className="cursor-pointer border-[0.1px] border-gray-400 p-4 px-10 rounded-lg hover:scale-125 transition transform duration-100 ease-out focus:outline-none focus:ring-2"
+                    onClick={login}
+                >
+                    Login
+                </button>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -3,14 +3,13 @@ import Header from "../components/Header";
 
 const Home = () => {
     return (
-        <div className="bg-[#272934] h-screen overflow-x-scroll scrollbar-hide">
+        <div className="h-screen overflow-x-scroll scrollbar-hide">
             <Head>
                 <title>JAK Discord Bot - Dashboard</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="">
-                <Header />
-            </div>
+            <Header />
+            <main className=""></main>
         </div>
     );
 };
