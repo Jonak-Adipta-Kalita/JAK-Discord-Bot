3599b881f6243b8183f5cceec814e7931c928cf4
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-17 11:31:01 +0530

    updated the response

--- src/cogs/8ball.py
+++ src/cogs/8ball.py
@@ -9,7 +9,7 @@ class _8Ball(commands.Cog):
             "It is certain.",
             "It is decidedly so.",
             "Without a doubt.",
-            "Yes – definitely.",
+            "Yes definitely.",
             "You may rely on it.",
             "As I see it, yes.",
             "Most likely.",
