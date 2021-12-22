4011463324c8e6b936e57bd4aa185f3261ec0c53
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-22 18:17:31 +0530

    i still dont know how to get the req.query.code

--- dashboard/pages/api/auth/[...nextauth].js
+++ dashboard/pages/api/auth/[...nextauth].js
@@ -1,20 +1,22 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export default NextAuth({
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET,
-            authorization: {
-                params: {
-                    scope: "identify guilds email",
+export default async function auth(req, res) {
+    return await NextAuth(req, res, {
+        providers: [
+            DiscordProvider({
+                clientId: process.env.DISCORD_CLIENT_ID,
+                clientSecret: process.env.DISCORD_CLIENT_SECRET,
+                authorization: {
+                    params: {
+                        scope: "identify guilds email",
+                    },
                 },
-            },
-        }),
-    ],
-    secret: process.env.JWT_SECRET,
-    pages: {
-        signIn: "/login",
-    },
-});
+            }),
+        ],
+        secret: process.env.JWT_SECRET,
+        pages: {
+            signIn: "/login",
+        },
+    });
+}
