b84c76de48662fdd6b9e88f722198e31973380f7
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-01-17 22:21:49 +0530

    created `name_fact_generate` command!!

--- src/core/files.py
+++ src/core/files.py
@@ -5,3 +5,9 @@ def code_snippet_file(carbon_file: str, author_id: int):
     file = disnake.File(carbon_file, filename=f"snippets/{author_id}.png")
 
     return file
+
+
+def name_fact_file(file: str, author_id: int):
+    file = disnake.File(file, filename=f"name_fact/{author_id}.png")
+
+    return file
