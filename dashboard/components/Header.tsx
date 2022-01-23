4cf0e8cfda5642f3df2c81310efc9d358db7aa06
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-01-23 21:50:41 +0530

    solved eslint problem

--- dashboard/components/Header.tsx
+++ dashboard/components/Header.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
