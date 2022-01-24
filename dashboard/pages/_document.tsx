894d2e732746a0e2c203718db8b6a88427d52e3a
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-01-24 10:04:58 +0530

    PWA!!

---
+++ dashboard/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/favicon.ico"></link>
+                    <meta name="theme-color" content="#272934" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
