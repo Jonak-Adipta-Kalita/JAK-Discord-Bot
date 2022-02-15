4940af1d8aa0a2ef6876d52e5170fd67c0aff516
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-15 12:56:21 +0530

    using separate directory!!

--- dashboard/src/pages/404.tsx
+++ dashboard/src/pages/404.tsx
@@ -1,19 +1,19 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const _500 = () => {
+const _404 = () => {
     return (
         <div className="flex flex-col h-screen bg-[#272934]">
             <Head>
-                <title>JAK Discord Bot | 500 Error</title>
+                <title>JAK Discord Bot | Page Not Found</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
             <main className="flex-1 overflow-y-auto scrollbar-hide bg-[#272934] px-2 md:px-4 lg:px-6 xl:px-10">
                 <div className="flex justify-center">
                     <h1 className="text-white font-bold text-2xl">
-                        500 Error!!
+                        Page Not Found!!
                     </h1>
                 </div>
             </main>
@@ -22,4 +22,4 @@ const _500 = () => {
     );
 };
 
-export default _500;
+export default _404;
--- dashboard/src/pages/500.tsx
+++ dashboard/src/pages/500.tsx
@@ -1,19 +1,19 @@
 import Head from "next/head";
-import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-const _404 = () => {
+const _500 = () => {
     return (
         <div className="flex flex-col h-screen bg-[#272934]">
             <Head>
-                <title>JAK Discord Bot | Page Not Found</title>
+                <title>JAK Discord Bot | 500 Error</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
             <main className="flex-1 overflow-y-auto scrollbar-hide bg-[#272934] px-2 md:px-4 lg:px-6 xl:px-10">
                 <div className="flex justify-center">
                     <h1 className="text-white font-bold text-2xl">
-                        Page Not Found!!
+                        500 Error!!
                     </h1>
                 </div>
             </main>
@@ -22,4 +22,4 @@ const _404 = () => {
     );
 };
 
-export default _404;
+export default _500;
