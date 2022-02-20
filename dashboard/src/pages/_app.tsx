783dd3ab2a86b3f9ca80133a3d5101686888a8a2
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-20 15:04:59 +0530

    the frontend is messed up?

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
-import { AppProps } from "next/dist/shared/lib/router/router";
+import Progressbar from "@badrap/bar-of-progress";
+import Router from "next/router";
+import type { AppProps } from "next/app";
+
+const progress = new Progressbar({
+    size: 4,
+    color: "#FD5B61",
+    className: "z-50",
+    delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 const MyApp = ({
     Component,
