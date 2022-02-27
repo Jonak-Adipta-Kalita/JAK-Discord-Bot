0f5b638b8de9bfc54dda96f63bcfdb608096e8a8
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-02-27 17:24:08 +0530

    the heck why its not working....

--- dashboard/src/pages/api/auth/[...nextauth].ts
+++ dashboard/src/pages/api/auth/[...nextauth].ts
@@ -50,33 +50,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 userGuildsRes.data.map((userGuild: Guild) => {
                     botGuildsRes.data.map(async (botGuild: Guild) => {
                         if (userGuild.id === botGuild.id && userGuild.owner) {
-                            const allowedRoles: Role[] = [];
+                            // const allowedRoles: Role[] = [];
 
-                            const rolesRes = await axios.get(
-                                `https://discord.com/api/v8/guilds/${userGuild.id}/roles`,
-                                {
-                                    headers: {
-                                        "Content-Type": "application/json",
-                                        Authorization: `Bot ${process.env.TOKEN}`,
-                                    },
-                                }
-                            );
+                            // const rolesRes = await axios.get(
+                            //     `https://discord.com/api/v8/guilds/${userGuild.id}/roles`,
+                            //     {
+                            //         headers: {
+                            //             "Content-Type": "application/json",
+                            //             Authorization: `Bot ${process.env.TOKEN}`,
+                            //         },
+                            //     }
+                            // );
 
-                            rolesRes.data.map((role: Role) => {
-                                if (
-                                    role.name !== "@everyone" &&
-                                    !role.managed
-                                ) {
-                                    allowedRoles.push(role);
-                                }
-                            });
-
-                            // commonGuilds.push({
-                            //     ...userGuild,
-                            //     roles: allowedRoles,
+                            // rolesRes.data.map((role: Role) => {
+                            //     if (
+                            //         role.name !== "@everyone" &&
+                            //         !role.managed
+                            //     ) {
+                            //         allowedRoles.push(role);
+                            //     }
                             // });
 
-                            commonGuilds.push(userGuild)
+                            commonGuilds.push({
+                                ...userGuild,
+                                // roles: rolesRes.data,
+                            });
                         }
                     });
                 });
--- dashboard/src/pages/dashboard.tsx
+++ dashboard/src/pages/dashboard.tsx
@@ -16,39 +16,51 @@ const Dashboard = () => {
             <Header />
             {session ? (
                 <div className="mx-auto mb-5 mt-3 space-y-4 md:mt-10 md:max-w-3xl lg:mt-[50px] lg:max-w-5xl">
-                    <div className="flex justify-center">
-                        <p className="my-[30px] text-3xl font-bold md:mt-0">
-                            Select a Server
-                        </p>
-                    </div>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                        {session?.user!.guilds?.map((guild) => (
-                            <div
-                                className="m-4 flex w-[250px] transform cursor-pointer flex-col items-center justify-center rounded-xl border-[0.2px] p-5 transition duration-100 ease-out hover:scale-105"
-                                key={guild.id}
-                                onClick={() =>
-                                    router.push(`/guild/${guild.id}`)
-                                }
-                            >
-                                {guild.icon ? (
-                                    <img
-                                        src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`}
-                                        alt=""
-                                        className="rounded-full"
-                                    />
-                                ) : (
-                                    <div className="rounded-full border-[0.2px] py-[46px] px-14">
-                                        <p className="text-4xl">
-                                            {guild.name[0]}
-                                        </p>
-                                    </div>
-                                )}
-                                <p className="mt-4 cursor-pointer">
-                                    {guild.name}
+                    {session?.user!.guilds?.length !== 0 ? (
+                        <>
+                            <div className="flex justify-center">
+                                <p className="my-[30px] text-3xl font-bold md:mt-0">
+                                    Select a Server
                                 </p>
                             </div>
-                        ))}
-                    </div>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                                {session?.user!.guilds?.map((guild) => (
+                                    <div
+                                        className="m-4 flex w-[250px] transform cursor-pointer flex-col items-center justify-center rounded-xl border-[0.2px] p-5 transition duration-100 ease-out hover:scale-105"
+                                        key={guild.id}
+                                        onClick={() =>
+                                            router.push(`/guild/${guild.id}`)
+                                        }
+                                    >
+                                        {guild.icon ? (
+                                            <img
+                                                src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`}
+                                                alt=""
+                                                className="rounded-full"
+                                            />
+                                        ) : (
+                                            <div className="rounded-full border-[0.2px] py-[46px] px-14">
+                                                <p className="text-4xl">
+                                                    {guild.name[0]}
+                                                </p>
+                                            </div>
+                                        )}
+                                        <p className="mt-4 cursor-pointer">
+                                            {guild.name}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                        </>
+                    ) : (
+                        <div className="mx-auto mt-5 flex justify-center md:mt-10 md:max-w-3xl lg:mt-[50px] lg:max-w-5xl">
+                            <p className="font-2xl font-bold">
+                                You are not a Owner of any Server, which have
+                                JAK Discord Bot in it!!
+                            </p>
+                        </div>
+                    )}
                 </div>
             ) : (
                 <div className="mx-auto mt-5 flex justify-center md:mt-10 md:max-w-3xl lg:mt-[50px] lg:max-w-5xl">
