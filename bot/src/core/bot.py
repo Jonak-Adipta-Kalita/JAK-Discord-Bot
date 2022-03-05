f5e9aac307ef71fb9f3d44d71707bcb0a552da87
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-03-05 13:17:17 +0530

    solved firebase issue thanks to pse :D

--- bot/src/core/bot.py
+++ bot/src/core/bot.py
@@ -36,26 +36,26 @@ class JAKDiscordBot(commands.Bot):
         print("Bot is Disconnected!!")
 
     async def on_ready(self):
-        # firebase_admin.initialize_app(
-        #     credential=firebase_admin.credentials.Certificate(
-        #         {
-        #             "type": credentials.FIREBASE_TYPE,
-        #             "project_id": credentials.FIREBASE_PROJECT_ID,
-        #             "private_key_id": credentials.FIREBASE_PRIVATE_KEY_ID,
-        #             "private_key": credentials.FIREBASE_PRIVATE_KEY,
-        #             "client_email": credentials.FIREBASE_CLIENT_EMAIL,
-        #             "client_id": credentials.FIREBASE_CLIENT_ID,
-        #             "auth_uri": credentials.FIREBASE_AUTH_URI,
-        #             "token_uri": credentials.FIREBASE_TOKEN_URI,
-        #             "auth_provider_x509_cert_url": credentials.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-        #             "client_x509_cert_url": credentials.FIREBASE_CLIENT_X509_CERT_URL,
-        #         }
-        #     )
-        # ) if not len(firebase_admin._apps) else firebase_admin.get_app()
+        firebase_admin.initialize_app(
+            credential=firebase_admin.credentials.Certificate(
+                {
+                    "type": credentials.FIREBASE_TYPE,
+                    "project_id": credentials.FIREBASE_PROJECT_ID,
+                    "private_key_id": credentials.FIREBASE_PRIVATE_KEY_ID,
+                    "private_key": credentials.FIREBASE_PRIVATE_KEY,
+                    "client_email": credentials.FIREBASE_CLIENT_EMAIL,
+                    "client_id": credentials.FIREBASE_CLIENT_ID,
+                    "auth_uri": credentials.FIREBASE_AUTH_URI,
+                    "token_uri": credentials.FIREBASE_TOKEN_URI,
+                    "auth_provider_x509_cert_url": credentials.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+                    "client_x509_cert_url": credentials.FIREBASE_CLIENT_X509_CERT_URL,
+                }
+            )
+        ) if not len(firebase_admin._apps) else firebase_admin.get_app()
 
-        # self.db: google.cloud.firestore_v1.client.Client = (
-        #     firebase_admin.firestore.client()
-        # )
+        self.db: google.cloud.firestore_v1.client.Client = (
+            firebase_admin.firestore.client()
+        )
 
         for type_, message in itertools.cycle(
             [
