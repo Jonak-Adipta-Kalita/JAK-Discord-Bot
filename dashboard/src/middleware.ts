a09f0d86e1cedb1a76198ae751605a0714be8a58
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-20 22:24:21 +0530

    its not working on the subdomain of my own domain... but on vercel's domain it works...

---
+++ dashboard/src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export const middleware = async (req: NextRequest) => {
+    const token = await getToken({ req, secret: process.env.JWT_SECRET! });
+    const { pathname } = req.nextUrl;
+
+    if (
+        !token &&
+        (pathname.includes("/dashboard") ||
+            pathname.includes("/guild") ||
+            pathname.includes("/suggestion"))
+    ) {
+        const url = req.nextUrl.clone();
+        url.pathname = "/login";
+
+        return NextResponse.rewrite(url);
+    }
+
+    if (token && pathname.includes("/login")) {
+        const url = req.nextUrl.clone();
+        url.pathname = "/";
+
+        return NextResponse.rewrite(url);
+    }
+};
