22cbc42984e7cb472c818d098136ffa9a6360a3f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-26 17:39:14 +0530

    lets get the channels

--- dashboard/src/types/typings.d.ts
+++ dashboard/src/types/typings.d.ts
@@ -6,6 +6,31 @@ export interface Command {
     description: string;
 }
 
+export interface Overwrite {
+    id: string;
+    type: number;
+    allow: string;
+    deny: string;
+}
+
+export interface User {
+    id: string;
+    username: string;
+    discriminator: string;
+    avatar: string;
+    bot: boolean;
+    system: boolean;
+    mfa_enabled: boolean;
+    banner: string;
+    accent_color: number;
+    locale: string;
+    verified: boolean;
+    email: string;
+    flags: number;
+    premium_type: number;
+    public_flags: number;
+}
+
 export interface Role {
     id: string;
     name: string;
@@ -27,6 +52,32 @@ export interface Guild {
     permissions: string;
     features: string[];
     roles: Role[];
+    channels: Channel[];
+}
+
+export interface Channel {
+    id: string;
+    type: number;
+    guild_id: string;
+    position: string;
+    permission_overwrites: Overwrite[];
+    name: string;
+    topic: string;
+    nsfw: boolean;
+    last_message_id: string;
+    bitrate: string;
+    user_limit: string;
+    rate_limit_per_user: string;
+    recipients: User[];
+    icon: string;
+    owner_id: string;
+    application_id: string;
+    parent_id: string;
+    last_pin_timestamp: string;
+    rtc_region: string;
+    video_quality_mode: string;
+    message_count: string;
+    member_count: string;
 }
 
 export type SelectedSiderbarOptions =
