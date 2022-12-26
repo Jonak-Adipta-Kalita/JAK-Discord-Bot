6e84119091463094e11b007b165aa19ec21300e4
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2022-12-26 14:40:25 +0530

    chat-gpt is amazing

--- dashboard/src/pages/api/auth/[...nextauth].ts
+++ dashboard/src/pages/api/auth/[...nextauth].ts
@@ -4,6 +4,22 @@ import DiscordProvider from "next-auth/providers/discord";
 import axios from "axios";
 import { Guild, Role } from "../../../types/typings";
 
+const getRoles = async (botGuild: Guild): Promise<Role[]> => {
+    const res = await axios.get<Role[]>(
+        `https://discord.com/api/v8/guilds/${botGuild.id}/roles`,
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bot ${process.env.TOKEN}`,
+            },
+        }
+    );
+
+    return res.data.filter(
+        (role: Role) => role.name !== "@everyone" && !role.managed
+    );
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     return await NextAuth(req, res, {
         providers: [
@@ -48,48 +64,35 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                         }
                     );
 
-                    userGuildsRes.data.map((userGuild: Guild) => {
-                        botGuildsRes.data.map((botGuild: Guild) => {
-                            if (
-                                userGuild.id === botGuild.id &&
-                                userGuild.owner
-                            ) {
-                                const allowedRoles: Role[] = [];
-
-                                axios
-                                    .get<Role[]>(
-                                        `https://discord.com/api/v8/guilds/${botGuild.id}/roles`,
-                                        {
-                                            headers: {
-                                                "Content-Type":
-                                                    "application/json",
-                                                Authorization: `Bot ${process.env.TOKEN}`,
-                                            },
-                                        }
-                                    )
-                                    .then((res) => {
-                                        res.data
-                                            .filter(
-                                                (role: Role) =>
-                                                    role.name !== "@everyone" &&
-                                                    !role.managed
-                                            )
-                                            .map((role: Role) =>
-                                                allowedRoles.push(role)
+                    const pushPromises = userGuildsRes.data.map(
+                        async (userGuild: Guild) => {
+                            return Promise.all(
+                                botGuildsRes.data.map(
+                                    async (botGuild: Guild) => {
+                                        if (
+                                            userGuild.id === botGuild.id &&
+                                            userGuild.owner
+                                        ) {
+                                            const roles = await getRoles(
+                                                botGuild
                                             );
+                                            return new Promise<void>(
+                                                (resolve) => {
+                                                    commonGuilds.push({
+                                                        ...botGuild,
+                                                        roles,
+                                                    });
+                                                    resolve();
+                                                }
+                                            );
+                                        }
+                                    }
+                                )
+                            );
+                        }
+                    );
 
-                                        // allowedRoles has roles
-                                    });
-
-                                // allowedRoles is empty
-
-                                commonGuilds.push({
-                                    ...botGuild,
-                                    roles: allowedRoles,
-                                });
-                            }
-                        });
-                    });
+                    await Promise.all(pushPromises);
 
                     return {
                         ...token,
